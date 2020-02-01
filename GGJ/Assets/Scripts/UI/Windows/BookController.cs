26c0743624985b0b7725f25d1603b63e2dfbeedc
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2020-02-02 00:28:26 +0200

    Book controller great fix!!!!!
    it works

---
+++ GGJ/Assets/Scripts/UI/Windows/BookController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BookController : MonoBehaviour
+{
+
+    public Button Right;
+    public Button Left;
+    public AudioClip sound;
+    public int page = 0;
+    public GameObject[] pages;
+
+    public void ButtonRight()
+    {
+        if (page < pages.Length-1)
+        {
+            page++;
+            Left.gameObject.SetActive(true);
+        }
+        if (page == pages.Length-1)
+            Right.gameObject.SetActive(false);
+    }
+
+    public void ButtonLeft()
+    {
+        if (page > 0)
+        {
+            page--;
+            Right.gameObject.SetActive(true);
+        }
+        if (page == 0)
+            Left.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        Right = Right.GetComponent<Button>();
+        Left = Left.GetComponent<Button>();
+        pages[0].SetActive(true);
+    }
+
+    public void Press(bool isRight)
+    {
+        //AudioSource.PlayClipAtPoint(sound, transform.position);
+        pages[page].SetActive(false);
+        if (isRight)
+            ButtonRight();
+        if (!isRight)
+            ButtonLeft();
+        pages[page].SetActive(true);
+
+    }
+
+}
