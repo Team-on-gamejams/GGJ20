a264beb8001b294895944a247d32b0ced3b78fbe
Team <timonsol13@gmail.com>
2020-02-01 21:38:45 +0200

    Patients

--- GGJ/Assets/Scripts/UI/TopRoom.cs
+++ GGJ/Assets/Scripts/UI/TopRoom.cs
@@ -11,12 +11,22 @@ public class TopRoom : MonoBehaviour
     [Header("Refs")]
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] Image patientSprite;
+    [SerializeField] GameObject closedObj;
 
     public void InitRoom(Patient patient) {
-        this.patient = patient;
+        if(patient == null) {
+            patientSprite.color = Color.clear;
+            closedObj.SetActive(true);
+        }
+        else {
+            this.patient = patient;
 
-        text.text = patient.docie.name + "\n";
-        text.text += patient.docie.neededOrgs + "\n";
-        patientSprite.sprite = patient.ui.sprite;
+            text.text = patient.docie.name + "\n";
+            text.text += patient.docie.neededOrgs + "\n";
+            patientSprite.color = Color.white;
+            patientSprite.sprite = patient.ui.sprite;
+
+            closedObj.SetActive(false);
+        }
     }
 }
