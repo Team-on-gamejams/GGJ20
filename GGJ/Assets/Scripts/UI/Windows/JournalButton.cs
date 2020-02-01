f592b08ad1bc6c0198fe4ba6aee632175549ef62
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2020-02-01 16:26:40 +0200

    new sprites and book added

---
+++ GGJ/Assets/Scripts/UI/Windows/JournalButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class JournalButton : MonoBehaviour
+{
+
+    public Button Right;
+    public Button Left;
+    public AudioClip sound;
+    public Animator ani;
+
+
+
+    void Start()
+    {
+        Right = Right.GetComponent<Button>();
+        Left = Left.GetComponent<Button>();
+        ani.GetComponent<Animator>();
+    }
+
+
+    public void Press(bool isRight)
+    {
+        //AudioSource.PlayClipAtPoint(sound, transform.position);
+        if (isRight)
+        {
+            Right.enabled = true;
+            if (ani.GetInteger("Page") < 2)
+                ani.SetInteger("Page", ani.GetInteger("Page") + 1);
+        }
+        if (!isRight)
+        {
+            Left.enabled = true;
+            if (ani.GetInteger("Page") > 0)
+                ani.SetInteger("Page", ani.GetInteger("Page") - 1);
+        }
+        
+    }
+}
