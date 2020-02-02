3a0ab0d2d3bd0f9d92133ec84b763b20da4e7bdd
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2020-02-02 14:45:17 +0200

    book fin

--- GGJ/Assets/Scripts/UI/Windows/DocieWindow.cs
+++ GGJ/Assets/Scripts/UI/Windows/DocieWindow.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DocieWindow : BaseWindow {
+
+	public GameObject text;
 	public override void ReInit(Patient patient) {
 		base.ReInit(patient);
 		
