629b8fc2eb392d777931c07fcd6e7161d8ac2b09
Team <timonsol13@gmail.com>
2020-02-02 13:15:47 +0200

    Organs

--- GGJ/Assets/Scripts/Enums.cs
+++ GGJ/Assets/Scripts/Enums.cs
@@ -3,4 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum Tools : byte { None, Scissors, Tweezers, Scalpel, Hammer, EndTool}
-public enum OrganTypes : short { None = 0, Org1 = 1, Org2 = 2, Org3 = 3, EndTool}
+public enum OrganTypes : short {
+    None = 0, 
+    Org1 = 1,
+    Org2 = 2, 
+    Org3 = 3, 
+    Heart = 4, 
+    Grusha = 5, 
+    Lungs = 6, 
+    Matka = 7, 
+    Bread = 8, 
+    Eye = 9, 
+    Brain = 10, 
+    Nokia = 11,
+    Heart2 = 12, 
+    EndOrgan
+}
