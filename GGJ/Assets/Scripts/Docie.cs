db7ca25d1c5bdf1e6b7c317fded0832f3f752a23
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2020-02-02 15:52:45 +0200

    Medic card

--- GGJ/Assets/Scripts/Docie.cs
+++ GGJ/Assets/Scripts/Docie.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 [Serializable]
 public class Docie
 {
-    public string name;
+    public string[] name;
     public string neededOrgs;
+    public string[] galaxy;
+    public string[] planet;
+    public string[] adress;
+    public string[] phone;
+    public string[] birth;
+    public string[] race;
+    public string[] gander;
+    public string[] sympthoms;
 }
--- GGJ/Assets/Scripts/UI/Windows/DocieWindow.cs
+++ GGJ/Assets/Scripts/UI/Windows/DocieWindow.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class DocieWindow : BaseWindow {
+public class DocieWindow : BaseWindow
+{
 
-	public GameObject text;
-	public override void ReInit(Patient patient) {
-		base.ReInit(patient);
-		
-	}
+    public Text txt;
+    public override void ReInit(Patient patient)
+    {
+        base.ReInit(patient);
+        txt.text = patient.docie.name[Random.Range(0, patient.docie.name.Length)]+"\n"+ patient.docie.galaxy[1]+"\n" + patient.docie.planet[1] + "\n" + patient.docie.adress[1] + "\n" + patient.docie.phone[1] + "\n" + patient.docie.birth[1] + "\n" + patient.docie.race[1] + "\n" + patient.docie.gander[1] + "\n" + patient.docie.sympthoms[1];
+    }
 }
