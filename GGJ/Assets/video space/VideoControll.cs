cc3f5ca549e7f2f62353f49026daa67942a879d8
Dima Shymko <trozeman@gmail.com>
2020-02-02 15:26:26 +0200

    Video on start

--- GGJ/Assets/video space/VideoControll.cs
+++ GGJ/Assets/video space/VideoControll.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoControll : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public VideoPlayer video;
     void Start()
     {
-        
+ 
+        // Debug.Log(video.frameCount);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    // Debug.Log( video.frame);
+
+        if(video.frame + 1 == (int)video.frameCount ){
+             video.gameObject.SetActive(false);
+        }
     }
 }
