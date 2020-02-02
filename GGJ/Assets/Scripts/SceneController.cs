172403f62b2cd7380cc44aa5e3a47887e1eca103
Dima Shymko <trozeman@gmail.com>
2020-02-02 04:12:38 +0200

    Pre build version

---
+++ GGJ/Assets/Scripts/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject loadingScreen;
+
+	public Slider loadBar;
+	
+	public Text progressText;
+
+	public int sceneID;
+
+	public void StartLoad()
+	{
+		loadingScreen.SetActive(true);
+		StartCoroutine(AsyncLoad());
+	}
+
+	IEnumerator AsyncLoad()
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+		while (!operation.isDone)
+		{
+			float progress = operation.progress / .9f;
+			loadBar.value = progress;
+			progressText.text = string.Format("{0:0}%", progress * 100);
+			yield return null;
+		}
+		
+	}
+}
