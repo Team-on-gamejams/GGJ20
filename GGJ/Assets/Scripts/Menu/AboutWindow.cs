47cea03063dc2729f9f27b7147eec759d49cadd4
Dima Shymko <trozeman@gmail.com>
2020-02-02 14:13:43 +0200

    Main menu

---
+++ GGJ/Assets/Scripts/Menu/AboutWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutWindow : MonoBehaviour
+{
+    public GameObject wnd;
+
+    public void showWnd(){
+        wnd.SetActive(true);
+    }
+    public void hideWnd(){
+        wnd.SetActive(false);
+    }
+}
--- GGJ/Assets/Scripts/SceneController.cs
+++ GGJ/Assets/Scripts/SceneController.cs
@@ -35,4 +35,8 @@ public class SceneController : MonoBehaviour
 		}
 		
 	}
+
+	public void Exit(){
+		Application.Quit();
+	}
 }
