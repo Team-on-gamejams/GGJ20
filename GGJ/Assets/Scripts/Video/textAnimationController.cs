c10a4744bee6e636df54bcbbde3cdfd04d12f753
Dima Shymko <trozeman@gmail.com>
2020-02-13 01:16:40 +0200

    Base text animation setup

---
+++ GGJ/Assets/Scripts/Video/textAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textAnimationController : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.Play("TextAnimation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            anim.Play("fadeOutTextAnimation");
+        }
+        
+        
+    }
+}
