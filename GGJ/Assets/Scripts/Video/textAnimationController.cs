e16fff737c9fb0def7f96e6abf899c7a4e6cc270
Dima Shymko <trozeman@gmail.com>
2020-02-13 14:52:11 +0200

    WIP video text animation

--- GGJ/Assets/Scripts/Video/textAnimationController.cs
+++ GGJ/Assets/Scripts/Video/textAnimationController.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class textAnimationController : MonoBehaviour
 {
     private Animator anim;
-
+    int q = 0;
+    public string[] texts;
+    public Text outText;
+    public float delay;
+    private bool isShow = false;
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
-        anim.Play("TextAnimation");
+        outText.text = texts[q];
+        InvokeRepeating("writeText", 1f, delay);
     }
 
-    // Update is called once per frame
+    
+    void writeText(){
+
+        if(q < texts.Length){
+            if(isShow){
+                anim.Play("fadeOutTextAnimation");             
+            }else{
+                outText.text = texts[q];
+                anim.Play("TextAnimation");
+                q++;
+            }
+            isShow = !isShow;
+        }           
+           
+    }
     void Update()
     {
         if (Input.GetKeyDown("space"))
